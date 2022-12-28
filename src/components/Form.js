f3aae857d5d1da6ce9d5768710e67dc471417f32
Subhajit Ghosh <subhajitstd07@gmail.com>
2022-12-28 09:57:38 +0530

    Added spinner

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,34 +3,40 @@ import ShowLocation from './ShowLocation';
 import SearchLocation from '../lib/SearchLocation';
 
 function Form() {
-    const locationinput = document.querySelector(".locationinput");
-    const showLocations = document.querySelector(".showLocations");
     const [data,setData] = useState("");
     const [loc,setLoc] = useState([]);
+    const [loading,setLoading] = useState(false)
+    const locationinput = document.querySelector(".locationinput");
+    const showLocations = document.querySelector(".showLocations");
     
     /**
      * 
      * @param {inputdata} e 
      */
-    const onDataChange = async (e) =>{
+    const oninputChange = async (e) =>{
         setData(e.target.value)
+        setLoading(true)
     }
 
     /**
      * Calling @SearchLocation funtion
      * this funtion takes location name
      */
-    const SearchMyLocation = async () =>{
+    const LoadLocations = async () =>{
         const datas = await SearchLocation(data);   
         if(datas){
             if(datas.data.length > 0){
-                setLoc(datas.data)
-                showLocations.style.display = "block";
+                setTimeout(()=>{
+                    setLoading(false)
+                    setLoc(datas.data)
+                    showLocations.style.display = "block";
+                },1000)
             }else{
                 console.log("No Location");
                 showLocations.style.display = "none";
             }
         }else{
+            setLoading(false)
             showLocations.style.display = "none";
         }
     }
@@ -39,28 +45,32 @@ function Form() {
      * 
      * @param {string} loc
      */
-    const clickSpecificLocation = (loc) =>{
-        console.log(loc);
+    const getSpecificLocation = (loc) =>{
         setData(loc);
         locationinput.value = loc;
         showLocations.style.display = "none";
     }
     
-    useEffect(()=>{
+    useEffect(()=>{ 
         if(data.length > 0){
-            SearchMyLocation();
+            LoadLocations();
         }
     },[data])
-  return (
+  return (  
     <div className='container'>
         <div className="mb-3 mt-5">
-            <input type="location" className="form-control locationinput" id="exampleFormControlInput1" placeholder="Enter your location" onChange={onDataChange}/>
+            <input type="location" className="form-control locationinput" id="exampleFormControlInput1" placeholder="Search for your location" onChange={oninputChange}/>
             <div className='showLocations' style={{"position":"relative","top":"2px"}}>
-                {loc.length > 0 ? loc.map((x,index)=>{
-                    return <ShowLocation key={index} searchedLocs={x.name} onClickData={()=> clickSpecificLocation(x.name)}/>
-                }) : ""}
+                {!loading ? loc.map((x,index)=>{
+                    return <ShowLocation key={index} searchedLocs={x.name} onClickData={()=> getSpecificLocation(x.name)}/>
+                }) : <ShowLocation  searchedLocs={<div className="spinner-border" role="status">
+                                                    <span className="sr-only"></span>
+                                                  </div>} />
+                }
             </div>
         </div>
+
+        
     </div>
   )
 }
